a1890e25e27e080624f42314b9b16a4daa484f40
Tongtong Liu <tongtong.liu@salesforce.com>
2018-06-28 22:29:19 -0700

    Initial Commit

---
+++ fbMessenger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fbMessenger
+//
+//  Created by Tongtong Liu on 6/28/18.
+//  Copyright © 2018 Tongtong Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
